633fa9a47bf9fb66ade145932f7f44c8554eefc4
Juliana <jlopker@gmail.com>
2016-03-21 16:06:09 -0700

    modifying starter code

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-  console.log("here's your mock data to start with:")
-  console.log(GLOBAL_MOCK_DATA_OBJECT)
-
-  // can you render the mock data to the page?
+$(document).on("ready", function(){
 
 });
